e58de98a7f4493d7869af92dbfb41b1b8f00afd7
student <pavelzamyatin333@gmail.com>
2024-02-26 16:45:34 +0300

    commit message

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from csv import reader, writer
+from csv import reader
 
 with open('products.csv', encoding='utf-8') as data_file:
     csv_data = reader(data_file, delimiter=';')
     result = 0
-
-    for row in csv_data:
-        if row[0] == 'Category':
-            row[6] += 'total'
-        elif row[0] != 'Яйца':
-            row[6] += int(row[3]) * int(row[4])
-        else:
-            row[6] += int(row[4]) * int(row[5])
+    for item in csv_data:
+        if item[0] == 'Закуски':
+            result += float(item[4]) * float(item[3])
+    print(result)
